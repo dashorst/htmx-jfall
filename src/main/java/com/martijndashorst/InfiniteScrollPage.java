acf1f3e3d371d72b7b7482062f1b1a05475d570c
Martijn Dashorst <martijn.dashorst@topicus.nl>
2023-10-29 23:43:01 +0100

    feat: add infinite scroll example

---
+++ src/main/java/com/martijndashorst/InfiniteScrollPage.java
@@ -0,0 +1,36 @@
+package com.martijndashorst;
+
+import java.util.List;
+
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+
+@Path("/infinitescroll")
+public class InfiniteScrollPage {
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance page(List<String> speakers);
+        public static native TemplateInstance speakers(List<String> speakers);
+    }
+
+    @Inject
+    Conference conference;
+
+    @GET
+    public TemplateInstance page() {
+        return Templates.page(conference.randomSpeakers(6));
+    }
+    @GET
+    @Path("/speakers")
+    public TemplateInstance speakers(@QueryParam("page") int pagenr) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        return Templates.speakers(conference.randomSpeakers(6));
+    }
+}
